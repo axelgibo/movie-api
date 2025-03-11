0bbff18bca754ecdcfb7a1f3ab98305d3fb2ba72
Chibron <axel.gibo.pro@gmail.com>
2025-03-11 18:27:04 +0100

    changed server.js

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
 const http = require("http");
 const url = require("url");
+const fs = require('fs');
 
 http
   .createServer((request, response) => {
     const parsedUrl = url.parse(request.url);
 
     if (parsedUrl.pathname.includes("documentation")) {
-      response.writeHead(200, { "Content-Type": "text/html" });
-      response.end("<h1>Documentation Page</h1>");
+      fs.readFile(__dirname + '/documentation.html', (err, data) => {
+        if (err) {
+          response.writeHead(500, { 'Content-Type': 'text/plain' });
+          response.end('Internal Server Error');
+        } else {
+          response.writeHead(200, { 'Content-Type': 'text/html' });
+          response.end(data);
+        }
+      });
     } else {
-      response.writeHead(200, { "Content-Type": "text/html" });
-      response.end("<h1>Index Page</h1>");
+      fs.readFile(__dirname + '/index.html', (err, data) => {
+        if (err) {
+          response.writeHead(500, { 'Content-Type': 'text/plain' });
+          response.end('Internal Server Error');
+        } else {
+          response.writeHead(200, { 'Content-Type': 'text/html' });
+          response.end(data);
+        }
+      });
     }
 
     const logMessage = `${new Date().toISOString()} - ${request.url}\n`;
