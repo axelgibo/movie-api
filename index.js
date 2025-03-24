8186f3c756ce31f3f8b67a11a1b9dd8398c11e0f
axelgibo <axel.gibo.pro@gmail.com>
2025-03-24 22:06:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -80,6 +80,17 @@ app.get(
   }
 );
 
+app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
 app.get(
   "/genres/:name",
   passport.authenticate("jwt", { session: false }),
