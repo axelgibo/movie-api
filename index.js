1b03517c0d26f1bc28526affc63441b574bd36af
axelgibo <axel.gibo.pro@gmail.com>
2025-04-06 20:08:30 +0200

    Added logging to /movies/:movieId

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ let allowedOrigins = [
 app.use(
   cors({
     origin: (origin, callback) => {
+      console.log("Origin:", origin);
       if (!origin) return callback(null, true);
       if (allowedOrigins.indexOf(origin) === -1) {
         // If a specific origin is not found on the list of allowed origins
@@ -64,15 +65,18 @@ app.get(
   }
 );
 
-app.get("/movies/:title", (req, res) => {
-  Movies.findOne({ Title: req.params.title })
-    .then((movie) => {
-      res.json(movie);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    });
+app.get("/movies/:movieId", (req, res) => {
+  console.log("movieId:", req.params.movieId); // Log the movieId
+
+  Movies.findById(req.params.movieId)
+      .then((movie) => {
+          console.log("Movie found:", movie); // Log the entire movie object
+          res.json(movie);
+      })
+      .catch((err) => {
+          console.error("Error finding movie:", err); // Log any errors
+          res.status(500).send("Error: " + err);
+      });
 });
 
 app.get(
